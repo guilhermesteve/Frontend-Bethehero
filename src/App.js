e3ca9952ac456318e68b0d6bd7b1bcf27b44167b
guilhermesteve <guilhermesoares134@gmail.com>
2020-03-29 17:35:44 -0300

    initialize frontend be the hero

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+import Routes from './routes';
+
+import './global.css';
+
+function App() {
+
+
+  return (
+    <>
+      <Routes />
+    </>
+  );
+}
+
+export default App;
